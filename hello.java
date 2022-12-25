a8cc134fac59d7056b01ae3bfe17fa14ee357a57
mzanawla <121326964+mzanawla@users.noreply.github.com>
2022-12-26 02:00:22 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello,all World in delhi!");
+        System.out.println("Hello,all World in delhi!");
     }
 }
