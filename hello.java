2bfaf1688ab018c8f8ce0a84a616eb0db6c92070
mzanawla <121326964+mzanawla@users.noreply.github.com>
2022-12-26 05:16:29 +0530

    Delete hello.java

--- hello.java
+++
@@ -1,6 +0,0 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello,all World in delhi!");
-        System.out.println("Hello,all World in delhi india!");
-    }
-}
